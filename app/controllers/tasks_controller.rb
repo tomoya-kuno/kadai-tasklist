afd2e3f4228f0b68977e199e6425e849c3e0fec3
tomoya.kuno2 <baymax_tomo_kame_marvel_513@docomo.ne.jp>
2020-08-10 15:07:41 +0000

    fix1-5

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,8 @@ class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
-    if logged_in?
-      @task = current_user.tasks.build
-      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
+    @task = current_user.tasks.build
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
