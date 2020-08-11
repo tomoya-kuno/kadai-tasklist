5de85742fd0ae1bb56fa4e08435dcf8e36d8c2ff
tomoya.kuno2 <baymax_tomo_kame_marvel_513@docomo.ne.jp>
2020-08-11 08:21:38 +0000

    fix2-1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,8 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
     @task = current_user.tasks.build
-    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
@@ -46,10 +44,6 @@ class TasksController < ApplicationController
   end
 
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
 
   def task_params
     params.require(:task).permit(:content,:status)
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
   
-  resources :tasks, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :tasks
 end
